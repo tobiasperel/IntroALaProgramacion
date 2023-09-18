c4e345b515e4f074c829da4c4d49368b3a51a0bb
tobiasperel <74215065+tobiasperel@users.noreply.github.com>
2023-09-18 12:50:17 -0300

    Update practica5.hs

--- guias/guia5Recursion/practica5.hs
+++ guias/guia5Recursion/practica5.hs
@@ -85,3 +85,53 @@ productoria [] = 1
 productoria [x] = x
 productoria (x:xs)
     |otherwise = x * productoria xs
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+compararTublas :: (String,String) -> (String,String) -> Bool
+compararTublas (a,b) (c,d)
+    | a == b || c == d =False
+    |(a,b) == (c,d) = False
+    |otherwise = True
+
+compararNormal :: (String,String) -> Bool
+compararNormal (a,b) 
+    | a == b = False
+    | otherwise = True
+
+relacionesValidas :: [(String,String)]-> Bool
+relacionesValidas [] = True   
+relacionesValidas x 
+    |longitud x == 1 && ((compararNormal (head x)) == False) = False
+    |longitud x == 1 && ((compararNormal (head x)) == True) = True
+
+relacionesValidas (x:xs) 
+    |relacionesValidas2 xs == False = False
+    | otherwise = relacionesValidas xs
+
+relacionesValidas2 :: [(String,String)]-> Bool
+relacionesValidas2 [] = True  
+relacionesValidas2 x 
+    |longitud x == 1 && ((compararNormal (head x)) == False) = False
+    |longitud x == 1 && ((compararNormal (head x)) == True) = True
+
+relacionesValidas2 (x:xs) 
+    |(compararTublas x (head xs) ) == False = False
+    |otherwise = relacionesValidas2 ([x] ++ (tail xs))
+
